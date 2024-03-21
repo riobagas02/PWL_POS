a5d94ea906316f8c555c3b0ad80c5d8bde55e0c2
Rio Bagas <riobagas02@gmail.com>
2024-03-21 23:21:32 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,5 +1,71 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Form Ubah Data User</title>
+    </head>
+    <body>
+        <h1>Form Ubah Data User</h1>
+        <a href="/user">Kembali</a>
+        <br><br>
+
+        <form method="post" action="/user/ubah_simpan/{{ $data->user_id }}">
+
+            {{ csrf_field() }}
+            {{ method_field('PUT') }}
+
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Masukan Username" value="{{ $data->username }}">
+            <br>
+            <label>Nama</label>
+            <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->username }}">
+            <br>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Masukan Password" value="{{ $data->password }}">
+            <br>
+            <label>Level ID</label>
+            <input type="number" name="level_id" placeholder="Masukan ID Level" value="{{ $data->level_id }}">
+            <br><br>
+            <input type="submit" class="btn btn-success" value="Ubah">
+            
+        </form>
+    </body>
+</html> --}}
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <a href="{{url ('/user/tambah')}}"> + Tambah User</a>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Aksi</td>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{$d->user_id}}</td>
+                    <td>{{$d->username}}</td>
+                    <td>{{$d->nama}}</td>
+                    <td>{{$d->level_id}}</td>
+                    <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
+
+{{-- <!DOCTYPE html>  --}}
+{{-- <html>
     <head>
         <title>Data User</title>
     </head>
@@ -12,14 +78,14 @@
                 <td>Nama</td>
                 <td>ID Level Pengguna</td>
             </tr>
-            {{-- @foreach ($data as $d) --}}
+            @foreach ($data as $d)
             <tr>
                 <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
-            </tr> 
-            {{-- @endforeach
+            </tr>
+            @endforeach --}}
 
             {{-- <tr>
                 <th>Jumlah Pengguna</th>
@@ -28,7 +94,7 @@
                 <td>
                     {{ $userCount }}
                 </td>
-            </tr> --}}
+            </tr> 
         </table>
     </body>
-</html>
+</html> --}}
