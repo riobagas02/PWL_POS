f1b55d5375792a0ef236f67bbf46d62d4179d60e
Rio Bagas <riobagas02@gmail.com>
2024-04-04 11:49:46 +0700

    Praktikum 2.7 - Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -221,11 +221,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
     public function tambah()
     {
         return view('user_tambah');
@@ -265,6 +265,14 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    // Praktikum 4
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        // dd($user);
+        return view('user', ['data' => $user]);
     }
 }
 
